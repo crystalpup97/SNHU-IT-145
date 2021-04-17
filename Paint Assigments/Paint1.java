12d246ea6ba5fafb75627113f8a82de86bcef8fb
crystalpup97 <82685665+crystalpup97@users.noreply.github.com>
2021-04-16 20:15:08 -0400

    Create Paint1.java

---
+++ Paint Assigments/Paint1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Paint1 {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        
+        final double squareFeetPerGallons = 350.0;
+        
+        // Implement a do-while loop to ensure input is valid
+        // Prompt user to input wall's height
+        do {
+        	try {
+        		System.out.println("Enter wall height (feet): ");
+        		wallHeight = scnr.nextDouble();
+        		if (wallHeight <= 0) {
+                    System.out.println("Invalid height");
+        		}
+        	}
+        	catch (Exception excpt) {
+        		System.out.println("Invalid input.");
+        		scnr.nextLine();
+        	}
+        }
+        while (wallHeight <= 0);
+
+        // Implement a do-while loop to ensure input is valid
+        // Prompt user to input wall's width
+        do {
+        	try {
+                System.out.println("Enter wall width (feet): ");
+                wallWidth = scnr.nextDouble();
+                if (wallHeight <= 0) {
+                	System.out.println("Invalid width");
+                }
+        	}
+            catch (Exception excpt) {
+            	 System.out.println("Invalid input");
+            	 scnr.nextLine();
+        	}
+        }
+        while (wallWidth <= 0);
+
+
+        // Calculate and output wall area
+        wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area: " + wallArea + " square feet");
+
+        // Calculate and output the amount of paint (in gallons) needed to paint the wall
+        gallonsPaintNeeded = wallArea/squareFeetPerGallons;
+        System.out.println("Paint needed: " + gallonsPaintNeeded + " gallons");
+
+    }
+}
